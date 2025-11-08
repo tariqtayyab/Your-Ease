5fe509461db8e558f96cb5e6266963f0486a9088
tariqtayyab <tariqtayyab229@gmail.com>
2025-11-08 13:24:21 +0500

    rr

--- your-ease/src/main.jsx
+++ your-ease/src/main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-// import "./index.css";
+import "./index.css";
 import { CartProvider } from "./context/CartContext";
 import ErrorBoundary from "./components/ErrorBoundary";
 
